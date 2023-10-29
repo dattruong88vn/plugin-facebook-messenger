26323fc04eb86ae9bc3c895f203d2850a56de7bc
dat.truong <dat.truong@secomm.vn>
2023-10-29 10:02:05 +0700

    upgrade version and change language

--- components/FacebookMessenger.js
+++ components/FacebookMessenger.js
@@ -7,7 +7,7 @@ function Facebook() {
     <div>
       <div id="fb-root"></div>
 
-      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <div id="fb-customer-chat" class="fb-customerchat"></div>
 
       <script
         dangerouslySetInnerHTML={{
@@ -19,19 +19,19 @@ function Facebook() {
       <script
         dangerouslySetInnerHTML={{
           __html: `window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml            : true,
-                  version          : 'v12.0'
-                });
-              };
-        
-              (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-                fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));`,
+              FB.init({
+                xfbml            : true,
+                version          : 'v18.0'
+              });
+            };
+
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));`,
         }}
       ></script>
     </div>
